ca94e1c33c2ff504c04b67d761d4aec17b66d8c8
DevMalls <94753949+DevMalls@users.noreply.github.com>
2022-07-17 02:09:10 +0530

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -65,7 +65,7 @@ const Footer = () => {
           </div>
         </Row>
 
-        <Carousel indicators="false">{getCarouselDetails()}</Carousel>
+        <Carousel indicators={false}> {getCarouselDetails()}</Carousel>
 
         {/*    ******Previous-Next-Icon*********
         
